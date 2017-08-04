582f3af448bf2c510fdc842a5afe43b92c01a086
Anthony 'Bitzawolf' Pepe <bitzah@gmail.com>
2017-08-04 12:09:13 -0700

    Refactored wolf more - changed forward movement, allow for traveling in all 4 directions

--- Scripts/Wolf/Wolf.cs
+++ Scripts/Wolf/Wolf.cs
@@ -16,6 +16,14 @@ public class Wolf : MonoBehaviour
         PAUSED
     }
 
+    public enum FacingDir
+    {
+        UP_RIGHT,
+        UP_LEFT,
+        DOWN_RIGHT,
+        DOWN_LEFT
+    }
+
     public State currentState = State.GRUONDED;
     [Tooltip("How long the player is in the dashing state where they cannot act.")]
     public float dashLengthTime = 0.5f;
@@ -25,12 +33,23 @@ public class Wolf : MonoBehaviour
     public float groundedBuffer = 0.15f;
     [Tooltip("How many air charges the player has at their disposal.")]
     public int maximumAirCharges;
+    [Tooltip("How many units/sec the player moves at the slowest speed.")]
+    public float runSpeed0 = 10;
+    public float runSpeed1 = 20;
+    public float runSpeed2 = 30; // fastest speed
+    [Tooltip("How many seconds the player must run at the given speed before upgrading to the next level.")]
+    public float runUpgradeTime = 5;
+    [Tooltip("How many units/sec^2 the player decelerates at.")]
+    public float speedDecelerationRate = 5;
+    [Tooltip("Force used to make the player jump.")]
+    public float jumpForceAmount = 100;
 
     public FMODUnity.StudioEventEmitter
         fm_running,
         fm_air;
 
     private State previousState;
+    private FacingDir facingDirection;
 
     private float
         aboutToLandCheckDistance,
@@ -38,7 +57,9 @@ public class Wolf : MonoBehaviour
         dashTimer,
         deathTimer,
         groundBufferTimer,
-        groundCheckDistance;
+        groundCheckDistance,
+        speed,
+        speedTierTimer;
 
     private bool
         aboutToLand, // Used to prevent animations from changing rapidly.
@@ -48,12 +69,18 @@ public class Wolf : MonoBehaviour
         forwardPressed,
         jumpPressed,
         leftPressed,
-        rightPressed;
+        rightPressed,
+        canTurn;
 
-    private int currentAirCharges;
+    [Header("Privates")]
+    [SerializeField]
+    private int
+        currentAirCharges,
+        currentRunTier;
 
     private Vector3
         vec_down,
+        vec_forward,
         vec_offset,
         vec_turn,
         vec_pauseVelocity;
@@ -65,11 +92,15 @@ public class Wolf : MonoBehaviour
     private void Start()
     {
         previousState = State.GRUONDED;
+        facingDirection = FacingDir.UP_RIGHT;
         aboutToLandCheckDistance = 20;
         airTimer = 0;
         dashTimer = 0;
+        deathTimer = 0;
         groundBufferTimer = 0;
         groundCheckDistance = 1;
+        speed = 0;
+        speedTierTimer = 0;
         aboutToLand = false;
         attackPressed = false;
         backwardPressed = false;
@@ -77,8 +108,11 @@ public class Wolf : MonoBehaviour
         forwardPressed = false;
         leftPressed = false;
         rightPressed = false;
+        canTurn = false;
         currentAirCharges =  maximumAirCharges;
+        currentRunTier = -1;
         vec_down = new Vector3(0, -1, 0);
+        vec_forward = -transform.right;
         vec_turn = new Vector3(0, 90, 0);
         vec_offset = new Vector3(0, 1, 0);
         vec_pauseVelocity = new Vector3();
@@ -104,8 +138,49 @@ public class Wolf : MonoBehaviour
     private void updateGrounded()
     {
         checkInputs();
-        // Move forward based on velocity/speed
-        // if no forward/backward pressed, slow down to a stop
+
+        if (forwardPressed && !backwardPressed)
+        {
+            speedTierTimer += Time.deltaTime;
+            checkAndUpgradeSpeedTier();
+            speed = getCurrentRunSpeedBasedOnTier();
+            transform.Translate(vec_forward * speed * Time.deltaTime);
+        }
+        if (backwardPressed && !forwardPressed)
+        {
+            //
+        }
+        if (!forwardPressed && !backwardPressed) // neither forward no backward pressed
+        {
+            if (speed > 0.1f)
+            {
+                speed -= speedDecelerationRate * Time.deltaTime;
+                if (speed <= 0.1f)
+                    speed = 0;
+                checkAndDowngradeSpeedTier();
+                transform.Translate(vec_forward * speed * Time.deltaTime);
+            }
+        }
+        if (jumpPressed)
+        {
+            rb.AddForce(transform.up * jumpForceAmount);
+        }
+        if (dashPressed)
+        {
+            transitionState(State.DASHING);
+        }
+        if (attackPressed)
+        {
+            // TODO activate hitbox and all.
+        }
+        if (leftPressed || rightPressed && canTurn)
+        {
+            //turn();
+        }
+
+        // set anim speed to current velocity.
+        // TODO if vel near 0, switch to idle anim at 1 speed.
+        checkForGroundAndTransition();
     }
 
     private void updateFalling()
@@ -120,6 +195,8 @@ public class Wolf : MonoBehaviour
 
         checkInputs();
         // slow down forward movement
+
+        checkForGroundAndTransition();
     }
 
     private void updateDashing()
@@ -219,6 +296,47 @@ public class Wolf : MonoBehaviour
     }
 
     /**
+     * Check to see if the player has met the requirements to run at the next highest speed tier.
+     * The requirements are to be running non-stop for some amount of time and for there to be a higher
+     * tier to move at.
+     */
+    private void checkAndUpgradeSpeedTier()
+    {
+        // going from a standstill is always an instant upgrade.
+        if (currentRunTier == -1)
+            ++currentRunTier;
+
+        // already at max tier
+        if (currentRunTier == 2)
+            return;
+
+        if (speedTierTimer >= runUpgradeTime)
+        {
+            ++currentRunTier;
+            speedTierTimer = 0;
+        }
+    }
+
+    /**
+     * Checks to see if the player's speed has dropped enough such that they're now at a slower speed tier.
+     * This function does not modify the speedTierTimer, which is used to -upgrade- the player's speed.
+     */
+    private void checkAndDowngradeSpeedTier()
+    {
+        bool downgradeTier = false;
+        switch (currentRunTier)
+        {
+            default:
+            case -1: return;
+            case 0: downgradeTier = speed < runSpeed0; break;
+            case 1: downgradeTier = speed < runSpeed1; break;
+            case 2: downgradeTier = speed < runSpeed2; break;
+        }
+        if (downgradeTier)
+            --currentRunTier;
+    }
+
+    /**
      * Checks to see if the wolf is on ground and changes the wolf's state to grounded or 
      * falling appropriately.
      * This function should only be called once in each state-update function.
@@ -281,19 +399,6 @@ public class Wolf : MonoBehaviour
     }
 
     /**
-     * Returns true if the player wants to and is in a state to be able to turn the intended direction.
-     * The player can only turn when they are on the ground (the GROUNDED state).
-     * @param direction True if the direction to check is left, false if right.
-     * @returns True if the player wants to (pressed the correct input) and is in a state to turn the queried
-     * direction.
-     */
-    public bool wantsToTurn(bool turnLeft)
-    {
-        bool inputPressed = (turnLeft && leftPressed) || (!turnLeft && rightPressed);
-        return (inputPressed && currentState == State.GRUONDED);
-    }
-
-    /**
      * Turns the wolf so now they are running in a new direction. Additionally,
      * the wolf is snapped into place at the specified position. This is used to keep the
      * player centered on the track since turn commands are only issued from invisible
@@ -308,6 +413,22 @@ public class Wolf : MonoBehaviour
 
         float mult = turnLeft ? -1 : 1;
         transform.Rotate(vec_turn * mult);
+        
+        switch (facingDirection)
+        {
+            case FacingDir.UP_RIGHT:   facingDirection = turnLeft ? FacingDir.UP_LEFT : FacingDir.DOWN_RIGHT; break;
+            case FacingDir.UP_LEFT:    facingDirection = turnLeft ? FacingDir.DOWN_LEFT : FacingDir.UP_RIGHT; break;
+            case FacingDir.DOWN_RIGHT: facingDirection = turnLeft ? FacingDir.UP_RIGHT : FacingDir.DOWN_LEFT; break;
+            case FacingDir.DOWN_LEFT:  facingDirection = turnLeft ? FacingDir.DOWN_RIGHT : FacingDir.UP_LEFT; break;
+        }
+
+        switch (facingDirection)
+        {
+            case FacingDir.UP_RIGHT:   vec_forward = -transform.right; break;
+            case FacingDir.UP_LEFT:    vec_forward =  transform.forward; break;
+            case FacingDir.DOWN_RIGHT: vec_forward = -transform.forward; break;
+            case FacingDir.DOWN_LEFT:  vec_forward =  transform.right; break;
+        }
         GameManager.i().cameraFollowing.SetOrientation(!turnLeft);
     }
 
@@ -347,5 +468,36 @@ public class Wolf : MonoBehaviour
     public void triggerLevelStarted()
     {
         transitionState(State.GRUONDED);
+    }
+
+    /**
+     * Returns the speed at which the player is currently running.
+     */
+    private float getCurrentRunSpeedBasedOnTier()
+    {
+        switch (currentRunTier)
+        {
+            default:
+            case -1: return 0f;
+            case 0: return runSpeed0;
+            case 1: return runSpeed1;
+            case 2: return runSpeed2;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "TurnPoint")
+        {
+            canTurn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "TurnPoint")
+        {
+            canTurn = false;
+        }
     }
 }
