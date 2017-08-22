0f41795f99d7a442e3dfb9f3f36da541f79c2dee
Anthony 'Bitzawolf' Pepe <bitzah@gmail.com>
2017-08-22 11:38:07 -0700

    Added code triggers to animate in and out the main menu.

--- Scripts/UI/MainMenu.cs
+++ Scripts/UI/MainMenu.cs
@@ -5,46 +5,115 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    GameManager gm;
+    public Animator buttonContainer;
+    public Animator titleContainer;
+    [Tooltip("How many seconds to wait inbetween triggering the title and then the button panel.")]
+    public float waitTime;
+
+    public delegate void FlyOutFinsihedEvent();
+    public event FlyOutFinsihedEvent OnAnimationOutFinish;
+
+    private enum State
+    {
+        ANIMATING_IN,
+        ANIMATING_OUT,
+        IDLE
+    }
+
+    private GameManager gm;
+    private State state = State.IDLE;
+    private bool animatingIn;
+    private bool initialized = false;
+    private float waitTimer = 0;
+
     private void Start()
     {
         gm = GameManager.i();
+        OnAnimationOutFinish += onFlyOutFinish;
     }
 
     public void show()
     {
-        animateIn();
         gameObject.SetActive(true);
+        waitTimer = 0;
+        state = State.ANIMATING_IN;
+
+        if (!initialized)
+        {
+            titleContainer.gameObject.SetActive(true);
+        }
+        else
+        {
+            titleContainer.SetTrigger("Fly Out");
+        }
     }
 
-    private void animateIn()
+    public void hide()
     {
-        // TODO animate in menu items
+        waitTimer = 0;
+        state = State.ANIMATING_OUT;
+        buttonContainer.SetTrigger("Fly Out");
     }
 
-    private void animateOut()
+    private void Update()
     {
-        // TODO animate out menu items
+        switch(state)
+        {
+            case State.ANIMATING_IN: updateAnimateIn(); break;
+            case State.ANIMATING_OUT: updateAnimateOut(); break;
+            case State.IDLE: break; // Do nothing
+        }
+    }
+
+    private void updateAnimateIn()
+    {
+        waitTimer += Time.deltaTime;
+        if (waitTimer >= waitTime)
+        {
+            if (!initialized)
+            {
+                buttonContainer.gameObject.SetActive(true);
+                initialized = true;
+            }
+            else
+            {
+                buttonContainer.SetTrigger("Fly Out");
+            }
+            state = State.IDLE;
+        }
+    }
+
+    private void updateAnimateOut()
+    {
+        waitTimer += Time.deltaTime;
+        if (waitTimer >= waitTime)
+        {
+            titleContainer.SetTrigger("Fly Out");
+            state = State.IDLE;
+            OnAnimationOutFinish();
+        }
     }
 
     public void onStartClicked()
     {
-        animateOut();
-        // TODO after animate out is done...
-        gm.loadingScreen.fadePanel.OnFinish += OnFadeinFinish;
-        gm.loadingScreen.fadeIn();
-        gm.playerWolf.setToLevelEndState();
+        hide();
     }
 
     public void onOptionsClicked()
     {
-        animateOut();
         // TODO show options menu. (Hey... What's -in- the options menu??)
     }
 
     public void onExitClicked()
     {
         Application.Quit();
+    }
+
+    private void onFlyOutFinish()
+    {
+        gm.loadingScreen.fadePanel.OnFinish += OnFadeinFinish;
+        gm.loadingScreen.fadeIn();
+        gm.playerWolf.setToLevelEndState();
     }
 
     /**
